d0404239edd241128705b103d1f7e6e966949a54
Haoran Huang <fshhr46@gmail.com>
2022-08-23 08:41:44 +0000

    add status api place holder

--- pytorch_version/app.py
+++ pytorch_version/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/status', methods=['GET'])
+def get_training_status():
+    job_id = request.args.get('id')
+    print(f"job id is {job_id}")
+    output = {
+        "running": True
+    }
+    return jsonify(output)
 
 
 if __name__ == '__main__':
